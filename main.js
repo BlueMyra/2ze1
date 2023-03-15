0b76c0e3981b53a9a26490f726cadf391e6c19e7
BlueMyra <127969245+BlueMyra@users.noreply.github.com>
2023-03-15 17:07:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+function phones(phone){
+    img.src = phone;;
+}
+function colors(colour){
+
+    container.style.background = colour;
+}
